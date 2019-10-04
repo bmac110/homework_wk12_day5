9c99fa310bfe7ea22bf64c6311ded417fa9763ed
Billy Mackay <bossmack50@gmail.com>
2019-10-04 13:36:29 +0100

    initial commit

---
+++ src/main/java/instruments/Guitar.java
@@ -0,0 +1,12 @@
+package instruments;
+
+public class Guitar {
+
+    private int numberOfStrings;
+
+    public Guitar(int numberOfStrings){
+        this.numberOfStrings = numberOfStrings;
+    }
+
+
+}
